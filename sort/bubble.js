33afc778e75c5a035575557f2cd5dd031a07e12f
linchuran <linchuran@guang.com>
2020-08-14 16:40:23 +0800

    <docs>: 添加注解 <refactory>: 抽离函数,优化可读性

--- sort/bubble.js
+++ sort/bubble.js
@@ -1,19 +1,21 @@
-function bubbleSort(arr) {
+/**
+ * 冒泡排序
+ * @param {Array} arr 待排序的数组
+ * @param {Array}
+ */
+export function bubbleSort(arr) {
   const len = arr.length;
+  const change = (i, j) => [arr[i], arr[j]] = [arr[j], arr[i]];
+
   for (let i = 0; i < len - 1; i++) {
     for (let j = 0; j < len - 1 - i; j++) {
-      if (arr[j] > arr[j + 1]) {        // 相邻元素两两对比
-        [
-          arr[j],
-          arr[j + 1],
-        ] = [
-          arr[j + 1],
-          arr[j]
-        ];
+
+      // 相邻元素两两对比
+      if (arr[j] > arr[j + 1]) {
+        change(j, j + 1);
       }
+
     }
   }
   return arr;
 }
-
-module.exports = bubbleSort;
